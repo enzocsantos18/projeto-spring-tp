eb424d3b56c34402af3bbe910bd5b97a24f4f706
Enzo de Carvalho Santos <enzo.santos.fcamara@fornecedores.klabin.com.br>
2021-10-28 20:12:14 -0300

    Página Receitas

---
+++ src/main/java/com/projeto/receitas/controller/ReceitaController.java
@@ -0,0 +1,22 @@
+package com.projeto.receitas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/receitas")
+public class ReceitaController {
+
+  @GetMapping
+  public String index() {
+    return "receitas/index";
+  }
+
+  @GetMapping("cadastro")
+  public String cadastro() {
+    return "receitas/cadastro";
+  }
+
+
+}
