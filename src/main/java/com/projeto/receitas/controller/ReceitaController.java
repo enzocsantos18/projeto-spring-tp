bfa36513f5a0a2edf32a60c5b60879098aa439e8
Camille Gachido <camillegachido70@gmail.com>
2021-11-18 23:06:06 -0300

    Adição de metodo que retornará a receita

--- src/main/java/com/projeto/receitas/controller/ReceitaController.java
+++ src/main/java/com/projeto/receitas/controller/ReceitaController.java
@@ -1,15 +1,18 @@
 package com.projeto.receitas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/receitas")
 public class ReceitaController {
 
-  @GetMapping
-  public String index() {
+  @GetMapping("/{id}")
+  public String index(@PathVariable("id") int id, Model model) {
+    model.addAttribute("nome", "TORTA <br> HOLANDESA");
     return "receitas/index";
   }
 
