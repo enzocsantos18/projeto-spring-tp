1db87de188b7795e1a923e018874c5dd5c515109
carolroc <carolroc02@gmail.com>
2021-11-29 01:07:40 -0300

    Busca com filtros

--- src/main/java/com/projeto/receitas/controller/HomeController.java
+++ src/main/java/com/projeto/receitas/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.projeto.receitas.controller;
 
 
+import com.projeto.receitas.service.OptionService;
 import com.projeto.receitas.service.ReceitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,29 +18,70 @@ public class HomeController {
   @Autowired
   private ReceitaService receitaService;
 
+  @Autowired
+  private OptionService optionService;
+
   @GetMapping
   public String index(Model model) {
-    List<Map<String, Object>> mapareceitas = receitaService.exibirReceitas();
+
+    List<Map<String, Object>> mapaComboCat = receitaService.categoriaCombo();
+    model.addAttribute("comboCats", mapaComboCat);
+
+    List<Map<String, Object>> mapaComboTag = optionService.exibirTags();
+    model.addAttribute("comboTags", mapaComboTag);
+
+    List<Map<String, Object>> mapaReceitas = receitaService.exibirTodasReceitas();
+    model.addAttribute("receitas", mapaReceitas);
+
+
+    return "index";
+  }
+
+  @GetMapping("/busca/{idCat}/{idTag}")
+  public String index(@PathVariable("idCat") int idCat,@PathVariable("idTag") int idTag, Model model) {
+
+    List<Map<String, Object>> mapaComboCat = receitaService.categoriaCombo();
+    model.addAttribute("comboCats", mapaComboCat);
+
+    List<Map<String, Object>> mapaComboTag = optionService.exibirTags();
+    model.addAttribute("comboTags", mapaComboTag);
+
+    //List<Map<String, Object>> mapaCombo = receitaService.categoriaCombo();
+    //model.addAttribute("combo", mapaCombo);
+
+    List<Map<String, Object>> mapareceitas = receitaService.exibirBusca(idCat,idTag);
     model.addAttribute("receitas", mapareceitas);
 
-    List<Map<String, Object>> mapaAside = receitaService.exibirReceitasTag(1);
-    model.addAttribute("asides", mapaAside);
+    //List<Map<String, Object>> mapareceitas = receitaService.exibirReceitasCategoria(idCat);
+    //model.addAttribute("receitas", mapareceitas);
 
+
+    //List<Map<String, Object>> mapareceitasTag = receitaService.exibirReceitasTag(idTag);
+    //model.addAttribute("receitasTag", mapareceitasTag);
 
     return "index";
   }
 
-  @GetMapping("/categoria/{id}")
-  public String index(@PathVariable("id") int id, Model model) {
-    List<Map<String, Object>> mapareceitas = receitaService.exibirReceitasCategoria(id);
-    
-    model.addAttribute("receitas", mapareceitas);
+  @PostMapping("/busca")
+    public String buscarPost(@RequestParam(value = "tags") Integer tags, @RequestParam(value = "categoria") Integer categoria, Model model) {
+      if (tags > 0) {
+        if (categoria > 0) {
+          return "redirect:/busca/" + categoria + "/" + tags;
+        }
+        else{
+          return "redirect:/busca/0/" + tags;
 
-    List<Map<String, Object>> mapaAside = receitaService.exibirReceitasTag(1);
-    model.addAttribute("asides", mapaAside);
-
-    return "index";
-  }
+        }
+      }
+      else{
+        if (categoria > 0) {
+          List<Map<String, Object>> mapareceitasCategoria = receitaService.exibirReceitasCategoria(categoria);
+          model.addAttribute("receitasCat", mapareceitasCategoria);
+          return "redirect:/busca/" + categoria + "/0";
+        }
+      }
+      return "redirect:/";
+    }
 
 
 
